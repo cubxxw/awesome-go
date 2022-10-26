67ce77c11f6f2a00f333380d402172006c64d6ca
xiongxinwei <3293172751nss@gmail.com>
2022-10-26 19:54:14 +0800

    new comit
    Signed-off-by: xiongxinwei <3293172751nss@gmail.com>

---
+++ docs/go-advancend/code/设计模式/1.go
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: xiongxinwei 3293172751nss@gmail.com
+ * @Date: 2022-10-26 19:44:38
+ * @LastEditTime: 2022-10-26 19:44:49
+ * @FilePath: \code\设计模式\1.go
+ * @Github_Address: https://github.com/3293172751/cs-awesome-Block_Chain
+ * Copyright (c) 2022 by xiongxinwei 3293172751nss@gmail.com, All Rights Reserved. @blog: http://nsddd.top
+ */
+package main
