b862d1004409da0a0614e3cd4ef63b9666ded1b8
xiongxinwei <3293172751nss@gmail.com>
2022-12-30 14:18:16 +0800

    docs(#1 about goalang black book): new
    Signed-off-by: xiongxinwei <3293172751nss@gmail.com>

--- docs/code/go-super/1-main.go
+++ docs/code/go-super/1-main.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
+var x float64 = 10 //这里的x是全局变量
+var y = 10
+
 func main() {
 	fmt.Println("hello world!")
+	fmt.Println("this is a test!")
+	if x := 1; x > 0 {
+		fmt.Println("x>0")
+	}
+	fmt.Println(x)
 
+	//打印x的类型
+	fmt.Printf("x type is %T\n", x)
+
+	//打印y的类型
+	fmt.Printf("y type is %T\n", y)
 }
