e2cfc69866a3598501b2c484d8aa774e299ebad3
xiongxinwei <3293172751nss@gmail.com>
2022-12-30 15:24:46 +0800

    vault backup: 2022-12-30 15:24:46

--- docs/code/go-super/1-main.go
+++ docs/code/go-super/1-main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 var x float64 = 10 //这里的x是全局变量
-var y = 10
+var y = 13
 
 func main() {
 	fmt.Println("hello world!")
@@ -18,4 +18,15 @@ func main() {
 
 	//打印y的类型
 	fmt.Printf("y type is %T\n", y)
+
+	//打印y的十六进制、八进制、二进制
+	fmt.Printf("y十六进制：%x\n", y)
+	fmt.Printf("y八进制：%o\n", y)
+	fmt.Printf("y二进制：%b\n", y)
+
+	//Fprintf和Sprintf区别：
+	//Fprintf是将格式化后的字符串输出到指定的io.writer接口类型的变量w中，返回写入的字节数和遇到的任何错误。
+	//Sprintf是将格式化后的字符串输出到指定的字符串变量str中，返回写入的字节数和遇到的任何错误。
+	//Fprintf和Sprintf的区别在于，Fprintf是将格式化后的字符串输出到指定的io.writer接口类型的变量w中，而Sprintf是将格式化后的字符串输出到指定的字符串变量str中。
+	//Fprintf和printf的区别在于，Fprintf是将格式化后的字符串输出到指定的io.writer接口类型的变量w中，而printf是将格式化后的字符串输出到标准输出中。
 }
